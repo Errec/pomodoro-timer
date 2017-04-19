33e954752d55b168a1afa1172dc6b173e22aa63b
Raniro Coelho <errecme@gmail.com>
2017-04-19 19:55:54 -0300

    Add bindEvent function to breakValue

--- js/breakInput.js
+++ js/breakInput.js
@@ -1,13 +1,17 @@
 var breakValue = {
-    breakValue: '1',
+    breakValue: 1,
     init: function() {
       this.cacheDom();
-      this.bindEvents(); //TODO
+      this.bindEvents();
       this.render(); //TODO
     },
     cacheDom: function () {
       this.breakInput = getElementById('break-input');
       this.incTime    = getElementById('break-increase');
       this.decTime    = getElementById('break-decrease');
+    },
+    bindEvents: function() {
+      this.incTime.on('click', this.increment.bind(this)); //TODO
+      this.decTime.on('click', this.decrement.bind(this)); //TODO
     }
 };
