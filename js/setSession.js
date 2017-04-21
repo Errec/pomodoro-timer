187ecff1e6a9ac045aadb4b369283163cca12016
Raniro Coelho <errecme@gmail.com>
2017-04-21 14:30:59 -0300

    Refactor setSession module into a revealing module one

--- js/setSession.js
+++ js/setSession.js
@@ -1,30 +1,35 @@
-var setSession = {
-    sessionValue: 1,
-    init: function() {
-      this.cacheDom();
-      this.bindEvents();
-      this.render();
-    },
-    cacheDom: function () {
-      this.sessionInput = document.getElementById('session-input');
-      this.incTime    = document.getElementById('session-increase');
-      this.decTime    = document.getElementById('session-decrease');
-    },
-    bindEvents: function() {
-      this.incTime.addEventListener('click', this.increment.bind(this));
-      this.decTime.addEventListener('click', this.decrement.bind(this));
-    },
-    render: function() {
-      this.sessionInput.textContent = this.sessionValue.toString();
-    },
-    increment: function() {
-      this.sessionValue === 360 ? '' : this.sessionValue++;
-      this.render();
-    },
-    decrement: function() {
-      this.sessionValue === 1 ? '' : this.sessionValue--;
-      this.render();
-    }
-};
+var setSession = (function(){
+  var sessionValue = 1;
+// Cache the DOM
+  var sessionInput = document.getElementById('session-input');
+  var incTime    = document.getElementById('session-increase');
+  var decTime    = document.getElementById('session-decrease');
+//  Bind click events
+  incTime.addEventListener('click', _increment.bind());
+  decTime.addEventListener('click', _decrement.bind());
+// Render the initial value from sessionValue
+  _render();
 
-setSession.init();
+  function _render() {
+    sessionInput.textContent = sessionValue.toString();
+  }
+
+  function _increment() {
+    sessionValue === 360 ? '' : sessionValue++;
+    _render();
+  }
+
+  function _decrement() {
+    sessionValue === 1 ? '' : sessionValue--;
+    _render();
+  }
+
+  function getValue () {
+     return sessionValue;
+  }
+
+  return {
+      getValue : getValue
+  };
+
+})();
