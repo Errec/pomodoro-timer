0a1290c2a0bcd5ece07d952445f7f2825a83acf1
Raniro Coelho <errecme@gmail.com>
2017-04-22 16:16:43 -0300

    Add bind click events on timer module

--- js/timer.js
+++ js/timer.js
@@ -1,8 +1,11 @@
 var timer = (function() {
-  var breakValue   = 5; //TODO
-  var sessionValue = 25;  //TODO
-
+  var breakValue   = 5; //TODO: add listener or coupling
+  var sessionValue = 25;  //TODO: add listener or coupling
+// Cache the DOM
   var timerDisplay = document.getElementById('timer-display');
   var start        = document.getElementById('start-btn');
   var reset        = document.getElementById('reset-btn');
+// Bind click events
+  start.addEventListener('click', _startTimer.bind());
+  reset.addEventListener('click', _resetTimer.bind());
 })();
