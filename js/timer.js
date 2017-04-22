501013464412f37d461a5a7bc57fed7e4fe6952c
Raniro Coelho <errecme@gmail.com>
2017-04-22 15:55:40 -0300

    Add var to cache the dom on timer module

--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,8 @@
 var timer = (function() {
   var breakValue   = 5; //TODO
   var sessionValue = 25;  //TODO
+
+  var timerDisplay = document.getElementById('timer-display');
+  var start        = document.getElementById('start-btn');
+  var reset        = document.getElementById('reset-btn');
 })();
