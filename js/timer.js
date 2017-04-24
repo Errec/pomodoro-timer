dfe55a7ac3177606f4408891bafd018acce5fda3
Raniro Coelho <errecme@gmail.com>
2017-04-24 20:44:35 -0300

    Refactor _startTime function to _runDisplay()

--- js/timer.js
+++ js/timer.js
@@ -1,31 +1,52 @@
 var timer = (function() {
-  var breakValue   = 5; //TODO: add listener or coupling
-  var sessionValue = 10;  //TODO: add listener or coupling
+  var breakValue   = 4; //TODO: add listener or coupling
+  var sessionValue = 7;  //TODO: add listener or coupling
+  var displayValue = sessionValue;
+  var playOn = true;
+  // var date = new Date();
 // Cache the DOM
   var timerDisplay = document.getElementById('timer-display');
   var start        = document.getElementById('start-btn');
   var reset        = document.getElementById('reset-btn');
 // Bind click events
-  start.addEventListener('click', _startTimer); //TODO: _startTimer
+  start.addEventListener('click', _runDisplay); //TODO: _startTimer
   reset.addEventListener('click', _resetTimer); //TODO: _resetTimer
 // Render the timer display
   _render();
 
   function _render() {
-    timerDisplay.textContent = sessionValue.toString();
-  }
-
-  function _startTimer() {
-    var sessionCountdown = setInterval(countdown, 1000);
+    timerDisplay.textContent = displayValue.toString();
   }
 
   function _resetTimer() {
     return;
   }
 
-function countdown() {
-  sessionValue--;
-  _render();
-}
+  function _runDisplay() {
+    var sessionTime = sessionValue;
+    var breakTime = breakValue;
+
+    var myCountdown = setInterval(function() {
+      if (sessionTime > 0) {
+      displayValue = --sessionTime;
+      _render();
+      }
+      else if (sessionTime === 0) {
+        if (breakTime > 0) {
+        displayValue = --breakTime;
+        _render();
+        }
+      }
+    }, 1000);
+
+    setTimeout(function (argument) {
+      clearTimeout(myCountdown);
+      if (playOn) {
+         _runDisplay();
+      } else {
+        return;
+      }
+    },(sessionValue + breakValue) * 1000);
+  }
 
 })();
