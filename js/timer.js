dcb213c7a378ed26e9b3224c2d7feae602733f78
Raniro Coelho <errecme@gmail.com>
2017-05-01 10:19:44 -0300

    Refactor play and reset btns into on btn

--- js/timer.js
+++ js/timer.js
@@ -6,17 +6,16 @@ var timer = (function() {
   var myBreakInnerCountdown = null;
   var loopTimer             = null;
   var disable               = false;
+  var playBtn               = true;
 // Cache the DOM
   var timerWrapper  = document.getElementById('timer-wrapper');
   var start         = document.getElementById('start-btn');
-  var reset         = document.getElementById('reset-btn');
   var sessionIncBtn = document.getElementById('session-increase');
   var sessionDecBtn = document.getElementById('session-decrease');
   var breakIncBtn   = document.getElementById('break-increase');
   var breakDecBtn   = document.getElementById('break-decrease');
 // Bind click events
-  start.addEventListener('click', _startCountdown);
-  reset.addEventListener('click', _resetTimer);
+  start.addEventListener('click', _countdownSwitch);
 // Render the timer display
 
 _updateNumberOfRectangles();
@@ -40,6 +39,16 @@ _updateNumberOfRectangles();
 
     type === 'session' ? rectangle.className += " rect-animate-color" : rectangle.className = rectangle.className.replace(reg,' ');
   }
+
+function _countdownSwitch() {
+  if(playBtn) {
+    playBtn = false;
+    _startCountdown();
+  } else {
+    playBtn = true;
+    _resetTimer();
+  }
+}
 
   function _resetTimer() {
     _disableBtns(false);
@@ -98,13 +107,11 @@ _updateNumberOfRectangles();
 
   function _disableBtns(disable) {
     if(disable === false) {
-      start.disabled         = false;
       sessionIncBtn.disabled = false;
       sessionDecBtn.disabled = false;
       breakIncBtn.disabled   = false;
       breakDecBtn.disabled   = false;
     } else {
-      start.disabled         = true;
       sessionIncBtn.disabled = true;
       sessionDecBtn.disabled = true;
       breakIncBtn.disabled   = true;
