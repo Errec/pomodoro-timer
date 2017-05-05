e3efb6946b909820e4788bee905cf6ed26816974
Raniro Coelho <errecme@gmail.com>
2017-05-05 16:38:23 -0300

    Add HackTime library to fix the alt tab interference on time functions

--- js/setSession.js
+++ js/setSession.js
@@ -1,5 +1,5 @@
 var setSession = (function(){
-  var sessionValue = 5;
+  var sessionValue = 0.2;
 // Cache the DOM
   var sessionInput = document.getElementById('session-input');
   var incTime      = document.getElementById('session-increase');
