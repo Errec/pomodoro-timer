30a6be2b2ab284d81086bb912034f19c73beaf92
Raniro Coelho <errecme@gmail.com>
2017-05-05 15:22:45 -0300

    Add headline time function update

--- js/timer.js
+++ js/timer.js
@@ -15,6 +15,8 @@ var timer = (function() {
   var sessionDecBtn = document.getElementById('session-decrease');
   var breakIncBtn   = document.getElementById('break-increase');
   var breakDecBtn   = document.getElementById('break-decrease');
+  var commitCount   = document.getElementById('commit-count');
+  var headline      = document.getElementById('headline');
 // Bind click events
   start.addEventListener('click', _countdownSwitch);
 // Render the timer display
@@ -61,6 +63,8 @@ _updateNumberOfRectangles();
     clearTimeout(myBreakCountdown);
     clearTimeout(myBreakInnerCountdown);
     clearTimeout(loopTimer);
+    clearTimeout(commitUpdate);
+    commitCount.textContent = 0;
     displayValue = setSession.getValue();
     for (var i = 0; i <= N_RECT; i++) {
       _render(i,'reset');
@@ -72,6 +76,8 @@ _updateNumberOfRectangles();
     var breakTime       = setBreak.getValue();
     var rectSessionTime = sessionTime / N_RECT;
     var rectBreakTime   = breakTime / N_RECT;
+    var commitTime      = rectSessionTime / 4;
+    var c               = 0;
     var s               = 0;
     var b               = 0;
     playOn              = true;
@@ -80,14 +86,21 @@ _updateNumberOfRectangles();
     _disableBtns(true);
     }
 
+    commitUpdate = setInterval(function() {
+      commitCount.textContent = c.toString() + ' contributions in the current session';
+      c++;
+    }, commitTime * minute);
+
     mySessionCountdown = setInterval(function() {
       if(s <= N_RECT) {
-      _render(s,'session');
-      s++;
+        _render(s,'session');
+        s++;
       }
     }, rectSessionTime * minute);
 
     myBreakCountdown = setTimeout(function () {
+      clearTimeout(commitUpdate);
+      commitCount.textContent = 'WARNING: the database server has gone away! Take a break...';
       myBreakInnerCountdown = setInterval(function() {
         if(b <= N_RECT) {
           _render(b,'break');
@@ -100,6 +113,7 @@ _updateNumberOfRectangles();
       clearTimeout(mySessionCountdown);
       clearTimeout(myBreakCountdown);
       clearTimeout(myBreakInnerCountdown);
+      clearTimeout(commitUpdate);
       if (playOn) {
          _startCountdown();
       } else {
